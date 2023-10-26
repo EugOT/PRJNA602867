ad692840e50e1c373d8a605f7c8248ed888631ed
Evgenii O. Tretiakov <Evgenii.Tretiakov@meduniwien.ac.at>
2023-10-26 16:08:13 +0000

    preprocessing

---
+++ code/cellranger_metrics_summary.R
@@ -0,0 +1,81 @@
+library(tidyverse)
+metrics_summary_SRR12391907 <- read_csv("/data/PRJNA602867/cellranger/SRR12391907/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391907")
+metrics_summary_SRR12391908 <- read_csv("/data/PRJNA602867/cellranger/SRR12391908/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391908")
+metrics_summary_SRR12391909 <- read_csv("/data/PRJNA602867/cellranger/SRR12391909/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391909")
+metrics_summary_SRR12391910 <- read_csv("/data/PRJNA602867/cellranger/SRR12391910/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391910")
+metrics_summary_SRR12391911 <- read_csv("/data/PRJNA602867/cellranger/SRR12391911/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391911")
+# metrics_summary_SRR12391912 <- read_csv("/data/PRJNA602867/cellranger/SRR12391912/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391912")
+metrics_summary_SRR12391913 <- read_csv("/data/PRJNA602867/cellranger/SRR12391913/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391913")
+metrics_summary_SRR12391914 <- read_csv("/data/PRJNA602867/cellranger/SRR12391914/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391914")
+metrics_summary_SRR12391915 <- read_csv("/data/PRJNA602867/cellranger/SRR12391915/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391915")
+metrics_summary_SRR12391916 <- read_csv("/data/PRJNA602867/cellranger/SRR12391916/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391916")
+metrics_summary_SRR12391917 <- read_csv("/data/PRJNA602867/cellranger/SRR12391917/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391917")
+metrics_summary_SRR12391918 <- read_csv("/data/PRJNA602867/cellranger/SRR12391918/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391918")
+metrics_summary_SRR12391919 <- read_csv("/data/PRJNA602867/cellranger/SRR12391919/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391919")
+# metrics_summary_SRR12391920 <- read_csv("/data/PRJNA602867/cellranger/SRR12391920/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391920")
+metrics_summary_SRR12391921 <- read_csv("/data/PRJNA602867/cellranger/SRR12391921/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391921")
+metrics_summary_SRR12391922 <- read_csv("/data/PRJNA602867/cellranger/SRR12391922/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391922")
+# metrics_summary_SRR12391923 <- read_csv("/data/PRJNA602867/cellranger/SRR12391923/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391923")
+# metrics_summary_SRR12391924 <- read_csv("/data/PRJNA602867/cellranger/SRR12391924/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391924")
+metrics_summary_SRR12391925 <- read_csv("/data/PRJNA602867/cellranger/SRR12391925/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391925")
+metrics_summary_SRR12391926 <- read_csv("/data/PRJNA602867/cellranger/SRR12391926/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391926")
+metrics_summary_SRR12391927 <- read_csv("/data/PRJNA602867/cellranger/SRR12391927/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391927")
+metrics_summary_SRR12391928 <- read_csv("/data/PRJNA602867/cellranger/SRR12391928/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391928")
+# metrics_summary_SRR12391929 <- read_csv("/data/PRJNA602867/cellranger/SRR12391929/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391929")
+metrics_summary_SRR12391930 <- read_csv("/data/PRJNA602867/cellranger/SRR12391930/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391930")
+metrics_summary_SRR12391931 <- read_csv("/data/PRJNA602867/cellranger/SRR12391931/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391931")
+metrics_summary_SRR12391932 <- read_csv("/data/PRJNA602867/cellranger/SRR12391932/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391932")
+metrics_summary_SRR12391933 <- read_csv("/data/PRJNA602867/cellranger/SRR12391933/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391933")
+metrics_summary_SRR12391934 <- read_csv("/data/PRJNA602867/cellranger/SRR12391934/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391934")
+metrics_summary_SRR12391935 <- read_csv("/data/PRJNA602867/cellranger/SRR12391935/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391935")
+metrics_summary_SRR12391936 <- read_csv("/data/PRJNA602867/cellranger/SRR12391936/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391936")
+metrics_summary_SRR12391937 <- read_csv("/data/PRJNA602867/cellranger/SRR12391937/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391937")
+metrics_summary_SRR12391938 <- read_csv("/data/PRJNA602867/cellranger/SRR12391938/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391938")
+metrics_summary_SRR12391939 <- read_csv("/data/PRJNA602867/cellranger/SRR12391939/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391939")
+metrics_summary_SRR12391940 <- read_csv("/data/PRJNA602867/cellranger/SRR12391940/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391940")
+# metrics_summary_SRR12391941 <- read_csv("/data/PRJNA602867/cellranger/SRR12391941/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391941")
+metrics_summary_SRR12391942 <- read_csv("/data/PRJNA602867/cellranger/SRR12391942/outs/metrics_summary.csv") %>% mutate(Run = "SRR12391942")
+metrics_summary <-
+  bind_rows(
+    metrics_summary_SRR12391907,
+    metrics_summary_SRR12391908,
+    metrics_summary_SRR12391909,
+    metrics_summary_SRR12391910,
+    metrics_summary_SRR12391911,
+    # metrics_summary_SRR12391912,
+    metrics_summary_SRR12391913,
+    metrics_summary_SRR12391914,
+    metrics_summary_SRR12391915,
+    metrics_summary_SRR12391916,
+    metrics_summary_SRR12391917,
+    metrics_summary_SRR12391918,
+    metrics_summary_SRR12391919,
+    # metrics_summary_SRR12391920,
+    metrics_summary_SRR12391921,
+    metrics_summary_SRR12391922,
+    # metrics_summary_SRR12391923,
+    # metrics_summary_SRR12391924,
+    metrics_summary_SRR12391925,
+    metrics_summary_SRR12391926,
+    metrics_summary_SRR12391927,
+    metrics_summary_SRR12391928,
+    # metrics_summary_SRR12391929,
+    metrics_summary_SRR12391930,
+    metrics_summary_SRR12391931,
+    metrics_summary_SRR12391932,
+    metrics_summary_SRR12391933,
+    metrics_summary_SRR12391934,
+    metrics_summary_SRR12391935,
+    metrics_summary_SRR12391936,
+    metrics_summary_SRR12391937,
+    metrics_summary_SRR12391938,
+    metrics_summary_SRR12391939,
+    metrics_summary_SRR12391940,
+    # metrics_summary_SRR12391941,
+    metrics_summary_SRR12391942
+  )
+
+metrics_summary |>
+  select("Estimated Number of Cells", "Run")
+
+write_tsv(metrics_summary, here::here("metrics_summary.tsv"))
